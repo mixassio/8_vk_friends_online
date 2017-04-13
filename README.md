4a98a467a0a20b0ff4c25cdad238bae48c1306f3
mixassio <mixail.bondarev@gmail.com>
2017-04-13 16:35:14 +0300

    first commit, run app

--- README.md
+++ README.md
@@ -1,6 +1,38 @@
 # Watcher of Friends Online
 
-[TODO. There will be project description]
+The script shows friends online in social network www.vk.com
+Enter login-password and put of list user online. 
+
+# HOW RUN
+
+Example of script launch on Linux, Python 3.6:
+
+```#!bash
+
+$ python vk_friends_online.py 
+
+$ Please, enter login:bigbear@list.ru
+$ Please, enter Password:
+List of my friends into online:
+-------------------------------
+1. Елена Смирнова
+2. Маргарита Швецова
+3. Константин Казаков
+4. Андрей Руцкин
+5. Ольга Логинова
+6. Марина Волкова
+7. Лена Яшина
+8. Tanya Morales
+9. Ирина Андропова
+10. Юрий Силкин
+11. Сергей Виноградов
+12. Юлия Кравцова
+13. Александр Сухарев
+14. Маргарита Дружинина
+15. Ольга Малистова
+16. Юлия Дудко
+17. Анна Петербуржская
+```
 
 # Project Goals
 
--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,29 +1,43 @@
-import vk
+import vk, getpass
 
-
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+APP_ID = '5969195'  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
 
 
 def get_user_login():
-    pass
+    login = input('Please, enter login:')
+    return login
 
 
 def get_user_password():
-    pass
+    password = getpass.getpass()
+    return password
 
 
 def get_online_friends(login, password):
+    my_dict = {}
+    list_user_online = []
     session = vk.AuthSession(
         app_id=APP_ID,
         user_login=login,
         user_password=password,
+        scope='friends'
     )
     api = vk.API(session)
-    # например, api.friends.get()
+    for user in api.friends.get(fields=('last_name')):
+        if user['online']:
+            my_dict['first_name'] = user['first_name']
+            my_dict['last_name'] = user['last_name']
+            list_user_online.append(my_dict.copy())
+    return list_user_online
 
 
 def output_friends_to_console(friends_online):
-    pass
+    print('List of my friends into online:')
+    print('-------------------------------')
+    i = 1
+    for user in friends_online:
+        print('{}. {} {}'.format(i, user['first_name'], user['last_name']))
+        i += 1
 
 if __name__ == '__main__':
     login = get_user_login()
