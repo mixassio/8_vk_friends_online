68eadd688f320bec0d1e8a04c9ee56130d5336de
mixassio <mixail.bondarev@gmail.com>
2017-04-14 17:22:10 +0300

    Corrected remarks of Lebedev

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,4 +1,5 @@
-import vk, getpass
+import vk
+import getpass
 
 APP_ID = '5969195'  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
 
@@ -23,24 +24,26 @@ def get_online_friends(login, password):
         scope='friends'
     )
     api = vk.API(session)
-    for user in api.friends.get(fields=('last_name')):
-        if user['online']:
-            my_dict['first_name'] = user['first_name']
-            my_dict['last_name'] = user['last_name']
-            list_user_online.append(my_dict.copy())
-    return list_user_online
+    list_users = api.friends.getOnline(online_mobile=1)
+    users_online = list(api.users.get(user_ids=list_users['online']))
+    users_online_mobile = api.users.get(user_ids=list_users['online_mobile'])
+    return users_online, users_online_mobile
 
 
-def output_friends_to_console(friends_online):
-    print('List of my friends into online:')
-    print('-------------------------------')
-    count_user = 1
-    for user in friends_online:
-        print('{}. {} {}'.format(count_user, user['first_name'], user['last_name']))
-        count_user += 1
+def output_friends_to_console(friends_online, friends_online_mobile):
+    print('----------------------------------------')
+    print('List of my friends into computer-online:')
+    print('----------------------------------------')
+    for count_user, user in enumerate(friends_online):
+        print('{}. {} {}'.format(count_user+1, user['first_name'], user['last_name']))
+    print('----------------------------------------')
+    print('List of my friends into mobile-online:')
+    print('----------------------------------------')
+    for count_user, user in enumerate(friends_online_mobile):
+        print('{}. {} {}'.format(count_user+1, user['first_name'], user['last_name']))
 
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    friends_online, friends_online_mobile = get_online_friends(login, password)
+    output_friends_to_console(friends_online, friends_online_mobile)
