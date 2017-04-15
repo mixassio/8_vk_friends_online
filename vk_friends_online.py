c3ac9ba36c3dc77dad2f8fe3da46978339efe597
mixassio <mixail.bondarev@gmail.com>
2017-04-15 11:21:00 +0300

    check remarks of Lebedev 14.04.2017

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -15,8 +15,6 @@ def get_user_password():
 
 
 def get_online_friends(login, password):
-    my_dict = {}
-    list_user_online = []
     session = vk.AuthSession(
         app_id=APP_ID,
         user_login=login,
@@ -24,26 +22,19 @@ def get_online_friends(login, password):
         scope='friends'
     )
     api = vk.API(session)
-    list_users = api.friends.getOnline(online_mobile=1)
-    users_online = list(api.users.get(user_ids=list_users['online']))
-    users_online_mobile = api.users.get(user_ids=list_users['online_mobile'])
-    return users_online, users_online_mobile
+    users_online = api.friends.getOnline()
+    return api.users.get(user_ids=users_online)
 
 
-def output_friends_to_console(friends_online, friends_online_mobile):
-    print('----------------------------------------')
-    print('List of my friends into computer-online:')
-    print('----------------------------------------')
+def output_friends_to_console(friends_online):
+    print('List of my friends online:')
+    print('--------------------------')
     for count_user, user in enumerate(friends_online):
         print('{}. {} {}'.format(count_user+1, user['first_name'], user['last_name']))
-    print('----------------------------------------')
-    print('List of my friends into mobile-online:')
-    print('----------------------------------------')
-    for count_user, user in enumerate(friends_online_mobile):
-        print('{}. {} {}'.format(count_user+1, user['first_name'], user['last_name']))
+
 
 if __name__ == '__main__':
     login = get_user_login()
     password = get_user_password()
-    friends_online, friends_online_mobile = get_online_friends(login, password)
-    output_friends_to_console(friends_online, friends_online_mobile)
+    friends_online = get_online_friends(login, password)
+    output_friends_to_console(friends_online)
